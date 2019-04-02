03f77c7e1b9a7e0d336d46ccbe8236ac5d8727ef
AlvinMotieram <47428010+AlvinMotieram@users.noreply.github.com>
2019-04-02 12:14:36 -0400

    Add files via upload

---
+++ week10/src/Calculate.java
@@ -0,0 +1,17 @@
+/*
+ * Author: Alvin
+ * Created: 4.2.19
+ * Desc: This class has calculate methods
+ */
+public class Calculate {
+	
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
+	public static double multiply(double a, double b) {
+		return a*b;
+	}
+	public static double getRectangleArea(double width, double length) {
+		return width*length;
+	}
+}
